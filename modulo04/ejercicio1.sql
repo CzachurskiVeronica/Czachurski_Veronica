1146622939b4c37f12dfb3204237e59e3f585c0d
Veronica Czachurski <137120296+CzachurskiVeronica@users.noreply.github.com>
2023-06-26 02:18:37 -0300

    Add files via upload

---
+++ modulo04/ejercicio1.sql
@@ -0,0 +1,43 @@
+CREATE TABLE ESTUDIANTE
+(
+  nombre VARCHAR(25) NOT NULL,
+  dni INT NOT NULL,
+  fecha_nac VARCHAR(25) NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  cod_matricula INT NOT NULL,
+  PRIMARY KEY (cod_matricula)
+);
+
+CREATE TABLE CURSO
+(
+  cod_curso INT NOT NULL,
+  nombre VARCHAR(25) NOT NULL,
+  PRIMARY KEY (cod_curso)
+);
+
+CREATE TABLE PROFESOR
+(
+  profesor_id INT NOT NULL,
+  nombre VARCHAR(25) NOT NULL,
+  especialidad VARCHAR(25) NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  PRIMARY KEY (profesor_id)
+);
+
+CREATE TABLE inscripto
+(
+  cod_matricula INT NOT NULL,
+  cod_curso INT NOT NULL,
+  PRIMARY KEY (cod_matricula, cod_curso),
+  FOREIGN KEY (cod_matricula) REFERENCES ESTUDIANTE(cod_matricula),
+  FOREIGN KEY (cod_curso) REFERENCES CURSO(cod_curso)
+);
+
+CREATE TABLE dicta
+(
+  profesor_id INT NOT NULL,
+  cod_curso INT NOT NULL,
+  PRIMARY KEY (profesor_id, cod_curso),
+  FOREIGN KEY (profesor_id) REFERENCES PROFESOR(profesor_id),
+  FOREIGN KEY (cod_curso) REFERENCES CURSO(cod_curso)
+);
---
+++ modulo04/ejercicio2.sql
@@ -0,0 +1,38 @@
+CREATE TABLE PAIS
+(
+  nombre_pais CHAR(25) NOT NULL,
+  pais_id INT NOT NULL,
+  PRIMARY KEY (pais_id)
+);
+
+CREATE TABLE PROVINCIA
+(
+  nombre_provincia CHAR(25) NOT NULL,
+  provincia_id INT NOT NULL,
+  pais_id INT NOT NULL,
+  PRIMARY KEY (provincia_id),
+  FOREIGN KEY (pais_id) REFERENCES PAIS(pais_id)
+);
+
+CREATE TABLE LOCALIDAD
+(
+  codigo_localidad INT NOT NULL,
+  nombre CHAR(25) NOT NULL,
+  codigo_postal INT NOT NULL,
+  provincia_id INT NOT NULL,
+  PRIMARY KEY (codigo_localidad),
+  FOREIGN KEY (provincia_id) REFERENCES PROVINCIA(provincia_id)
+);
+
+CREATE TABLE EMPLEADO
+(
+  empleado_id INT NOT NULL,
+  dni INT NOT NULL,
+  nombre CHAR(25) NOT NULL,
+  telefono INT NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  _fecha_alta VARCHAR(25) NOT NULL,
+  codigo_localidad INT NOT NULL,
+  PRIMARY KEY (empleado_id),
+  FOREIGN KEY (codigo_localidad) REFERENCES LOCALIDAD(codigo_localidad)
+);
