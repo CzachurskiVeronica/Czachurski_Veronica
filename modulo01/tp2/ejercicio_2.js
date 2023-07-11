e381352004a02721b50e3b9d82b8f45ce9c20c8e
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-11 20:40:47 -0300

    Modificacion del ejercicio 2, agregado de comentarios

--- modulo01/tp2/ejercicio_2.js
+++ modulo01/tp2/ejercicio_2.js
@@ -1,17 +1,19 @@
-// Completar las condiciones de los IF del siguiente script para que los mensajes se muestren siempre
-// de forma correcta:
+/* Completar las condiciones de los IF del siguiente script para que los mensajes se muestren siempre
+   de forma correcta: */
 
-var num1 = 3;
-var num2 = 7;
-if (num1 < num2) {
+var num1 = 3; //Declara una variable local e inicializa con el valor adjudicado
+var num2 = 7; //Declara una variable local e inicializa con el valor adjudicado
+
+//Evalua cada una de las posibilidades
+if (num1 < num2) {  //Si el primer valor ingresado es menor al segundo
     console.log("num1 no es mayor que num2");
 } 
-if (num2 > 0) {
+if (num2 > 0) { //Si el segundo valor ingresado es mayor a 0
     console.log("num2 es positivo");
 }
-if (num1 < 0) {
+if (num1 < 0) { //Si el primer valor ingresado es menor a 0
     console.log("num1 es negativo o distinto de cero");
 }
-if (num1 + 1 != num2) {
+if (num1 + 1 != num2) { //Si el primer valor + 1 es distinto del segundo valor
     console.log("Incrementar en 1 unidad el valor de num1 no lo hace mayor o igual que num2");
 }
