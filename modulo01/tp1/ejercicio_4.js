1596b951d0ded343f3bb376b24205c32308bce45
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-11 15:19:31 -0300

    Modificacion del ejercicio N 4

--- modulo01/tp1/ejercicio_4.js
+++ modulo01/tp1/ejercicio_4.js
@@ -2,13 +2,13 @@
 
 const A = "Hola"; //Define el valor de una variable A que sea de tipo constante
 let B = " mundo!"; //Define el valor de una variable local B 
-document.write("Constante A contiene " + A ); //Muestra el contenido de la constante
-document.write("</br>"); //Efectúa un salto de linea
+document.write("Constante A contiene " + A );  /* Muestra el contenido de la constante, concatenando con el
+                                                  signo + el valor que contiene la variable A */
+document.write("</br>"); //Efectúa un salto de linea con la etiqueta </br>
 document.write("Variable B contiene " + B ); //Muestra el contenido de la variable B
 document.write("</br>"); //Efectúa un salto de linea
 document.write(A + B ); //Permite concatenar ambas cadenas de texto
 document.write("</br>"); //Efectúa un salto de linea
-document.write("La etiqueta BR es un salto de linea");
+document.write("La etiqueta BR es un salto de linea"); //Muestra el siguiente mensaje
 document.write("</br>"); //Efectúa un salto de linea
 document.write("El signo '+' sirve para concatenar cadenas de texto");
-
