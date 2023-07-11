e5705ae98b712ee33b908b79bdac1ae648fbd1f9
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-11 15:26:58 -0300

    Modificacion del ejercicio. Agregado de comentarios

--- modulo01/tp1/ejercicio_6.js
+++ modulo01/tp1/ejercicio_6.js
@@ -1,8 +1,10 @@
-// Crear un array llamado “dias” y que almacene el nombre de los siete días de la semana. Mostrar por
-// pantalla los siete nombres utilizando la función console.log().
+/* Crear un array llamado “dias” y que almacene el nombre de los siete días de la semana. Mostrar por
+   pantalla los siete nombres utilizando la función console.log(). */
 
 var dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves ", "Viernes", "Sábado"];
-//Declara un array que se llama dias, la cual contiene diferentes valores.
+//Declara un array que se llama dias, la cual contiene diferentes valores y es variable.
 
-//Con dicha funcion permite mostrar en la consola cada valor del Array ingresado.
+/* Con dicha funcion permite mostrar en la consola cada valor del Array ingresado.
+   Describiendo a su vez que la misma tiene una longitud de 7 elementos,
+   cada una de los mismos en un valor diferente asignado */
 console.log(dias);
