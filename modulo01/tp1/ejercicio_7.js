702abfbc6419cfda4f1af88138e8a76c6779d0a9
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-11 15:30:19 -0300

    Agregado de comentarios de mejor explicacion al trabajo

--- modulo01/tp1/ejercicio_7.js
+++ modulo01/tp1/ejercicio_7.js
@@ -1,5 +1,5 @@
-// Crear el archivo correspondiente a “ejercicio_7.js”, Ejecutarlo y luego agregar lineas de comentario
-// explicando que hace el programa
+/* Crear el archivo correspondiente a “ejercicio_7.js”, Ejecutarlo y luego agregar lineas de comentario
+   explicando que hace el programa */
 
 let dato, resultado; 
 val1 = window.prompt("Introduce tu nombre: "); 
@@ -8,9 +8,9 @@ resultado = `Concatenado tu nombre y apellido es: ${val1} ${val2} ` ;
 document.write(resultado);
 
 
-//El programa utiliza la función window.prompt() para solicitar el ingreso de los valores solicitados
-//Declara dos variables dato y resultado, sin embargo la variable dato puede ser eliminada, ya que en el fragmento de codigo no se la utiliza.
-//Muestra una ventana emergente y solicita el ingreso del dato, almacenando en val1
-//Muestra una ventana emergente y solicita el ingreso del dato, almacenando en val2
-//Crea una cadena de texto, empleando los valores ingresados. la cual se almacena en la variable resultado.
-//Utiliza la función document.write() para mostrar el contenido de la variable resultado en el navegador.
+/* 1- Declara dos variables dato y resultado, sin embargo la variable dato puede ser eliminada, ya que en el fragmento de codigo no se la utiliza.
+   2- El programa utiliza la función window.prompt() para solicitar el ingreso de los valores solicitados.
+        Muestra una ventana emergente y solicita el ingreso del dato, almacenando en val1
+        Muestra una ventana emergente y solicita el ingreso del dato, almacenando en val2
+   3- Crea una cadena de texto, empleando los valores ingresados. la cual se almacena en la variable resultado.
+   4- Utiliza la función document.write() para mostrar el contenido de la variable resultado en el navegador. */
