9f3d0b58b032850479a501d1dd95f0ebe344d4b1
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-11 15:14:50 -0300

    Cambios efectuados para mejor comprension del ejercicio

--- modulo01/tp1/ejercicio_1.js
+++ modulo01/tp1/ejercicio_1.js
@@ -1,8 +1,8 @@
-// 1) Crear el primer archivo llamado “ejercicio_1.js” y pegar el siguiente texto en su interior:
-// 2) agregar un comentario debajo que explique que hace esa primer linea de código
+/* 1) Crear el primer archivo llamado “ejercicio_1.js” y pegar el siguiente texto en su interior:
+   2) agregar un comentario debajo que explique que hace esa primer linea de código */
 
 document.write("Hola Mundo!");
 
-//Dicha linea de código al ejecutarse con el comando document.write
-//es una función que permite mostrar cadenas de caracteres en la pagina web de HTML.
-//Es decir, que es posible de ser visualizado por parte del usuario. 
+/* Dicha linea de código al ejecutarse con el comando document.write
+   es una función que permite mostrar cadenas de caracteres en la pagina web de HTML.
+   Es decir, que es posible de ser visualizado por parte del usuario. */
--- modulo01/tp1/ejercicio_3.js
+++ modulo01/tp1/ejercicio_3.js
@@ -1,11 +1,10 @@
 // Copiar el siguiente código dentro de un archivo, luego guardarlo con el nombre: “ejercicio_3.js”.
 
-let num1=0; //Declara una variable local num1 y le asigna el valor 0
-let num2=0; //Declara una variable local num2 y le asigna el valor 0
-num1 = num1 + 3; //Al valor de la variable inicial, realiza una operación suma,
-                // reservándolo en la variable inicial, modificando su valor
-num2 = num2 + 7; //Al valor de la variable inicial, realiza una operación suma,
-                // reservándolo en la variable inicial, modificando su valor
+let num1=0; //Declara una variable local num1 y le asigna valor 0
+let num2=0; //Declara una variable local num2 y le asigna valor 0
+num1 = num1 + 3; /*Al valor de la variable inicial num1, realiza una operación suma,
+                   reservándolo en la variable inicial, modificando su valor */
+num2 = num2 + 7; /*Al valor de la variable inicial num2, realiza una operación suma,
+                   reservándolo en la variable inicial, modificando su valor */
 alert (`El primer numero ahora es ${num1} `); //Muestra el resultando de ambas operaciones efectuadas
 alert (`El segundo numero ahora es ${num2} ` );
-
