0d0292a9f9c78266f3da26cc8ad1861214605efd
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-12 20:44:50 -0300

    Creacion de archivo .sql con inserts de ejercicio2

---
+++ modulo04/inserts_ejercicio2.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO ejercicio_2.pais (nombre_pais, pais_id) VALUES ("Turquia", 239), ("Dinamarca", 458), ("Francia", 793);
+
+-- INSERT INTO ejercicio_2.provincia (`nombre_provincia`,`provincia_id`,`pais_id`) VALUES ('Burdeos',2548,239), ('Zakopane',4582,458), ('Coimbra',5693,793);
+
+-- INSERT INTO ejercicio_2.localidad (codigo_localidad, nombre, codigo_postal, provincia_id)  VALUES  (224, "Bourdeux", 1469, 5693), (479, "Noruega", 2364, 5693), (1423, "Moscu", 4236, 2548);
+
+-- INSERT INTO ejercicio_2.empleado (empleado_id, dni, nombre, telefono, email, _fecha_alta, codigo_localidad) VALUES (148, 45965352, "Rodolfo Rojas", 457831252, "rodolfo@gmail.com", '2012-02-14', 224),  (159, 24863425, "Marta Gonzalez", 45963257, "marta@gmail.com", '2011-05-24', 479), (1936, 24569823, "Gabriel Sexton", 14582362, "gabriel@gmail.com", '2022-09-15', 479);
