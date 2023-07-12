d9d7dd4798ba9c66820f9d997db98bec4ef1723c
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-12 16:20:11 -0300

    Modificacion del ejercicio num 8

--- modulo01/tp2/ejercicio_8.js
+++ modulo01/tp2/ejercicio_8.js
@@ -1,3 +1,17 @@
-// parseint multiplicar por 100 tambien
-Crea una función que genere 100 números aleatorios entre 1 y 100 que no se repitan y luego
-imprima por pantalla usando document.write().
+/* Crea una función que genere 100 números aleatorios entre 1 y 100 que no se repitan y luego
+imprima por pantalla usando document.write(). */
+
+function generarNumeros() { //funcion para generar numeros aleatorios
+  var numeros = new Set();
+
+  while (numeros.size < 100) { //Genera numeros hasta 100
+    var numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    numeros.add(numeroAleatorio); //Verifica si los numeros creados no se repiten y permite ir cargandolos
+  }
+
+  numeros.forEach(function(numero) {
+    document.write(numero + "|" + ' ');
+  });
+}
+
+generarNumeros(); //Invoca la funcion
