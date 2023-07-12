adb627c6b6a6e6d387b3bce44abbfaf54526f7ce
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-11 21:44:32 -0300

    Modificacion del ejercicio num 5. Agregado de una evaluacion en la funcion y de comentarios en referencia a la misma

--- modulo01/tp2/ejercicio_5.js
+++ modulo01/tp2/ejercicio_5.js
@@ -1,10 +1,17 @@
-// Crear una función que muestre información sobre una cadena de texto que se le pasa como
-// argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada
-// sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+/* Crear una función que muestre información sobre una cadena de texto que se le pasa como
+   argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada
+   sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas. */
 
-console.log(determinarTipoCadena("HOLA")); 
+let ingreso = prompt("Ingrese una cadena de texto: "); //Solicita el ingreso por teclado de la cadena de texto
+
+console.log(determinarCadena(ingreso)); //Muestra por consola el resultado de la funcion
 
 function determinarCadena(cadena) {
+  let valor = /\d/.test(cadena); /*La expresion permite evluar si contiene un digito numerico
+                                    retornando un valor booleano true o false */
+  if (valor == true) { //En caso de que lo resultante de la evaluacion sea true, devuelve el siguiente mensaje
+    return "No se puede evaluar. Usted debe ingresar una cadena de texto unicamente";
+  } else if (valor != true) { //En caso de que lo resultante sea distinto de true, evalua cada una de las posibilidades
     if (cadena === cadena.toUpperCase()) {
       return "Mayúsculas";
     } else if (cadena === cadena.toLowerCase()) {
@@ -13,3 +20,4 @@ function determinarCadena(cadena) {
       return "Mezcla de mayúsculas y minúsculas";
     }
   }
+}
