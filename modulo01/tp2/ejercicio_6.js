e01916720d0626989a8d81b856ca2ae2315532e4
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-12 00:01:21 -0300

    Modificacion del ejercicio num 6. Codigo y comentarios agregados

--- modulo01/tp2/ejercicio_6.js
+++ modulo01/tp2/ejercicio_6.js
@@ -1,26 +1,28 @@
-// Escribir un script que muestre la posición de la primera vocal de un texto introducido por
-// teclado. Por ejemplo: perro = “e” es la letra Nº2 ; árbol = “a” es la letra Nº1.
+/* Escribir un script que muestre la posición de la primera vocal de un texto introducido por
+   teclado. Por ejemplo: perro = “e” es la letra Nº2 ; árbol = “a” es la letra Nº1. */
 
-// Función para determinar si un carácter es una vocal
-function esVocal(caracter) {
-    return /["aeiou"]/i.test(caracter);
+// Función para determinar si un carácter es una vocal, adjudicando el valor 
+function vocal(caracter) {
+  return /["aeiou"]/i.test(caracter);
+}
+
+// Solicita al usuario ingresar un texto por teclado
+var texto = prompt("Ingresa un texto:");
+
+// Busca la posición de la primera vocal en el texto
+var posicion = -1;
+
+//Funcion que recorre el texto ingresado por el usuario
+for (var escrito = 0; escrito < texto.length; escrito++) { //Evalua que la longitud sea menor al valor inicializado
+  if (vocal(texto[escrito])) { //Mediante la invocacion a la funcion anterior, la invoca para determinar la existencia de una vocal
+    posicion = escrito + 1; // Suma 1 para que la posición sea legible (comenzando desde 1)
+    break;
   }
-  
-  // Solicitar al usuario ingresar un texto por teclado
-  var texto = prompt("Ingresa un texto:");
-  
-  // Buscar la posición de la primera vocal en el texto
-  var posicion = -1;
-  for (var i = 0; i < texto.length; i++) {
-    if (esVocal(texto[i])) {
-      posicion = i + 1; // Sumar 1 para que la posición sea legible (comenzando desde 1)
-      break;
-    }
-  }
-  
-  // Mostrar el resultado
-  if (posicion !== -1) {
-    console.log("La primera vocal es: " + texto[posicion - 1] + " en la posicion Nº" + posicion);
-  } else {
-    console.log("No se encontro ninguna vocal en el texto.");
-  }
+}
+
+// Muestra el resultado
+if (posicion !== -1) {
+  console.log("La primera vocal es: " + texto[posicion - 1] + " en la posicion Nº" + posicion);
+} else {
+  console.log("No se encontro ninguna vocal en el texto.");
+}
