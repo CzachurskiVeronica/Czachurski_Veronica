4fc656cb63517bd83d38d7469ef050e1d1397ebe
CzachurskiVeronica <czachurski.veronica@gmail.com>
2023-07-12 21:16:28 -0300

    Carga de datos, inserts de ejercicio1

---
+++ modulo04/inserts_ejercicio1.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO ejercicio_1.estudiante (nombre, dni, fecha_nac, email, cod_matricula) VALUES ("Dalma Maradona", 11895457, '1995-05-08', 'dalma@gmail.com', 1189), ('Florencia Gonzalez', 25458796, '1994-05-20', 'florencia@gmail.com', 2545), ('Emilia Florez', 42189534, '1992-03-12', 'emilia@gmail.com', 4218);
+
+-- INSERT INTO ejercicio_1.profesor (profesor_id, nombre, especialidad, email) VALUES (1458, 'Martin Dimitri', 'Lic en Matematica', 'dimitri@gmail.com'), (5482, 'Carlos Sanchez', 'Lic en Filosofia', 'sanchez@gmail.com'), (4785, 'Martina Ruiz', 'Lic en Sistemas', 'ruiz@gmail.com');
+
+-- INSERT INTO ejercicio_1.curso (cod_curso, nombre) VALUES (148, 'Matematica'), (574, 'Sistemas'), (789, 'Filosofia');
